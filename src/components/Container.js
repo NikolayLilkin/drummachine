ced26ae2a8cfc7d877b4dc7d09a050bd94520a78
Nikolay Anatoliev Lilkin <nikolayanatolievlilkin@gmail.com>
2022-03-23 19:44:11 +0200

    commit

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,12 +2,13 @@ import { Component } from "react";
 
 class Container extends Component{
     render(){
+
         return (
-            <div className="container" id="container">
-                <div className="row justify-content-md-center">
-                    <div className="col-md d-inline-flex m-20">
+            <div className="container d-flex align-items-center justify-content-center" id="container">
+                <div className="container row justify-content-center p-0">
+                    <div className="col d-inline-flex">
                         <div className="col d-flex flex-column">
-                            <div className="d-flex flex-row justify-content-md-center">
+                            <div className="col d-flex flex-row justify-content-center">
                                 <div className="p-1">
                                     <button className="btn btn-primary">Q</button>
                                 </div>
@@ -18,7 +19,7 @@ class Container extends Component{
                                     <button className="btn btn-primary">E</button>
                                 </div>
                             </div>
-                            <div className="d-flex flex-row justify-content-md-center">
+                            <div className="col d-flex flex-row justify-content-center">
                                 <div className="p-1">
                                     <button className="btn btn-primary">A</button>
                                 </div>
@@ -29,7 +30,7 @@ class Container extends Component{
                                     <button className="btn btn-primary">D</button>
                                 </div>
                             </div>
-                            <div className="d-flex flex-row justify-content-md-center">
+                            <div className="col d-flex flex-row justify-content-center">
                                 <div className="p-1">
                                     <button className="btn btn-primary">Z</button>
                                 </div>
@@ -42,16 +43,21 @@ class Container extends Component{
                             </div>
                         </div>
                     <div className="col d-inline-flex m-20">
-                        <div className="col d-flex flex-column justify-content-center">
-                            <div class="form-check form-switch d-flex flex-column">
-                                <label class="form-check-label" for="Power">Power</label>
-                                <input class="form-check-input" type="checkbox" id="Power"/>
+                        <div className="col d-flex flex-column justify-content-center" id="settings">
+                            <div className="d-flex flex-row justify-content-center form-switch form-check" id="PowerDiv">
+                                <input className="form-check-input" type="checkbox" id="Power"/>
+                                <label className="form-check-label" style={{marginLeft:10}} for="Power">Power</label>
                             </div>
-                            <label for="range" class="form-label">Range</label>
-                            <input type="range" class="form-range w-50" id="range"/>
-                            <div class="form-check form-switch d-flex flex-column">
-                                <label class="form-check-label" for="Bank">Bank</label>
-                                <input class="form-check-input" type="checkbox" id="Bank"/>
+                            <div className="d-flex flex-column justify-content-center" id="SoundDiv">
+                                <p>Heater</p>
+                            </div>
+                            <div className="d-flex flex-column justify-content-center" id="RangeDiv">
+                                <label for="range" className="form-label">Range</label>
+                                <input type="range" className="form-range" id="range"/>
+                            </div>
+                            <div className="d-flex flex-row justify-content-center form-switch form-check" id="BankDiv">
+                                <input className="form-check-input" type="checkbox" id="Bank"/>
+                                <label className="form-check-label" style={{marginLeft:10}} for="Bank">Bank</label>
                             </div>
                         </div>
                     </div>
