fc5cbd927a17b1b9874211be4c57e5c8b979f327
Nikolay Anatoliev Lilkin <nikolayanatolievlilkin@gmail.com>
2022-03-23 13:55:05 +0200

    added Bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
       <h1>This is a drummachine</h1>
+      <button type="button" class="btn btn-primary">Primary</button>
     </div>
   );
 }
